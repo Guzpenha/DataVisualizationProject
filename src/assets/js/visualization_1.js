232307ec720d1920a67a8cf90f3185756bedcef8
Gustavo Penha <guzpenha@Guz-notebook>
2017-06-08 19:34:24 -0300

    visualization 3 interation

--- src/assets/js/visualization_1.js
+++ src/assets/js/visualization_1.js
@@ -1,71 +1,133 @@
 function plot_visualization_1(){
+  // Interation functionality 
+  // var svgSlider = d3.select("#sliderForPerplexity")
+  //     margin = {right: 50, left: 50},
+  //     width = +svgSlider.attr("width") - margin.left - margin.right,
+  //     height = +svgSlider.attr("height");
 
-// set the dimensions and margins of the graph
-var margin = {top: 50, right: 20, bottom: 30, left: 60},
-    width = 300 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom;
+  // svgSlider.append("text")
+  //   .text("t-sne Perplexity:")
+  //   .attr("x", 10)
+  //   .attr("y", 60)
+  //   .attr("font-family", "sans-serif")
+  //   .attr("font-size", "18px")
+  //   .attr("fill", "black");
+  // var x = d3.scaleLinear()
+  //     .domain([5, 50])
+  //     .range([0, width])
+  //     .clamp(true);
 
-// set the ranges
-var x = d3.scaleLinear().range([0, width]);
-var y = d3.scaleLinear().range([height, 0]);
+  // var slider = svgSlider.append("g")
+  //     .attr("class", "slider")
+  //     .attr("transform", "translate(" + margin.left + "," + height / 2 + ")");
 
-var color = d3.scaleOrdinal()
-  .domain(["0", "1"])
-  .range(["#b5d1ff", "#c10108" ]);
+  // slider.append("line")
+  //     .attr("class", "track")
+  //     .attr("x1", x.range()[0])
+  //     .attr("x2", x.range()[1])
+  //   .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+  //     .attr("class", "track-inset")
+  //   .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+  //     .attr("class", "track-overlay")
+  //     .call(d3.drag()
+  //         .on("start.interrupt", function() { slider.interrupt(); })
+  //         .on("start drag", function() { position(x.invert(d3.event.x)); }));
 
-svgs = []
-for (i = 0 ;i <17 ;i++){
-  $("#question3").append("<div id=\"vis3plot"+i+"\" class =\"multiplePlots\"></div>")
-  var svg = d3.select("#vis3plot"+i).append("svg").attr("id", "vis3svg"+i)
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-  svgs.push(svg)
-}
+  // slider.insert("g", ".track-overlay")
+  //     .attr("class", "ticks")
+  //     .attr("transform", "translate(0," + 18 + ")")
+  //     .selectAll("text")
+  //     .data(x.ticks(10))
+  //     .enter().append("text")
+  //     .attr("x", x)
+  //     .attr("text-anchor", "middle")
+  //     .text(function(d) { return d});
 
-recommenders = ["BiasedMatrixFactorization","BiPolarSlopeOne","CoClustering","Constant","Constant5","FactorWiseMatrixFactorization","GlobalAverage","ItemAverage","LatentFeatureLogLinearModel","MatrixFactorization","Random","SigmoidSVDPlusPlus","SlopeOne","TimeAwareBaseline","TimeAwareBaselineWithFrequencies","UserAverage","UserItemBaseline"]
+  // var handle = slider.insert("circle", ".track-overlay")
+  //     .attr("class", "handle")
+  //     .attr("r", 9);
 
-// Get the data
-d3.csv("../data/user_2d.csv", function(error, dataAll) {
-  if (error) throw error;
+  // function position(h) {
+  //   perplexity = String(Math.floor(h))
+  //   drawScatterPlots("50");  
+  //   value = Math.max(Math.min(x(h),300),0)  
+  //   handle.attr("cx", value);  
+  // }
 
-  for (var i = 0; i < recommenders.length; i++) {
-    rec = recommenders[i]
-    data = dataAll.filter(function(r){return r.RS ===rec}) 
-
-    svgs[i].append("text")
-          .attr("x", (width / 2))             
-          .attr("y", 0 - (margin.top / 2))
-          .attr("text-anchor", "middle")  
-          .style("font-size", "16px") 
-          // .style("text-decoration", "underline")  
-          .text(rec);
-
-    // Scale the range of the data
-    x.domain(d3.extent(data, function(d) { return d.TSNE_0; }));
-    y.domain(d3.extent(data, function(d) { return d.TSNE_1; }));
-
-    // Add the scatterplot
-    svgs[i].selectAll("dot")
-        .data(data)
-        .enter().append("circle")
-        .attr("r", 3)
-        .style("fill-opacity", .6)
-        .attr("cx", function(d) { return x(d.TSNE_0); })
-        .attr("cy", function(d) { return y(d.TSNE_1); })
-        .style("fill", function(d) { return color(d.label); });
-
-    // Add the X Axis
-    svgs[i].append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
-
-    // Add the Y Axis
-    svgs[i].append("g")
-        // .attr("transform", "translate("+width+",0)")
-        .call(d3.axisLeft(y));
-    }
-});
+  drawScatterPlots("50");
 
 }
+
+function drawScatterPlots(perp){
+  $("#question3").empty()
+  // set the dimensions and margins of the graph
+  var margin = {top: 50, right: 20, bottom: 30, left: 60},
+      width = 300 - margin.left - margin.right,
+      height = 300 - margin.top - margin.bottom;
+
+  // set the ranges
+  var x = d3.scaleLinear().range([0, width]);
+  var y = d3.scaleLinear().range([height, 0]);
+
+  var color = d3.scaleOrdinal()
+    .domain(["0", "1"])
+    .range(["#b5d1ff", "#c10108" ]);
+
+
+  svgs = []
+  for (i = 0 ;i <17 ;i++){
+    $("#question3").append("<div id=\"vis3plot"+i+"\" class =\"multiplePlots\"></div>")
+    var svg = d3.select("#vis3plot"+i).append("svg").attr("id", "vis3svg"+i)
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    svgs.push(svg)
+  }
+
+  recommenders = ["BiasedMatrixFactorization","BiPolarSlopeOne","CoClustering","Constant","Constant5","FactorWiseMatrixFactorization","GlobalAverage","ItemAverage","LatentFeatureLogLinearModel","MatrixFactorization","Random","SigmoidSVDPlusPlus","SlopeOne","TimeAwareBaseline","TimeAwareBaselineWithFrequencies","UserAverage","UserItemBaseline"]
+  
+  // Get the data
+  d3.csv("../data/user_2d.csv", function(error, dataAll) {
+    if (error) throw error;
+    dataAll = dataAll.filter(function(r){return r.perplexity ===perp})
+    for (var i = 0; i < recommenders.length; i++) {
+      rec = recommenders[i]
+      data = dataAll.filter(function(r){return r.RS ===rec})
+      // data = data.filter(function(r){return r.perplexity ===perplexity})
+
+      svgs[i].append("text")
+            .attr("x", (width / 2))             
+            .attr("y", 0 - (margin.top / 2))
+            .attr("text-anchor", "middle")  
+            .style("font-size", "16px") 
+            // .style("text-decoration", "underline")  
+            .text(rec);
+
+      // Scale the range of the data
+      x.domain(d3.extent(data, function(d) { return d.TSNE_0; }));
+      y.domain(d3.extent(data, function(d) { return d.TSNE_1; }));
+
+      // Add the scatterplot
+      svgs[i].selectAll("dot")
+          .data(data)
+          .enter().append("circle")
+          .attr("r", 3)
+          .style("fill-opacity", .6)
+          .attr("cx", function(d) { return x(d.TSNE_0); })
+          .attr("cy", function(d) { return y(d.TSNE_1); })
+          .style("fill", function(d) { return color(d.label); });
+
+      // Add the X Axis
+      svgs[i].append("g")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x));
+
+      // Add the Y Axis
+      svgs[i].append("g")
+          // .attr("transform", "translate("+width+",0)")
+          .call(d3.axisLeft(y));
+      }
+  });
+}
+
