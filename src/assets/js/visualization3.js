d7a0ad72cd6ca39d96b32eed72f58ee1a71baf87
guz <guzpenha10@gmail.com>
2017-06-27 19:07:03 -0300

    adicionando interacao na vis 3

--- src/assets/js/visualization3.js
+++ src/assets/js/visualization3.js
@@ -14,7 +14,7 @@ function drawScatterPlots(perp){
     .range(["#b5d1ff", "#c10108" ]);
 
 
-  svgs = []
+  svgs = []  
   for (i = 0 ;i <17 ;i++){
     $("#question3").append("<div id=\"vis3plot"+i+"\" class =\"multiplePlots\"></div>")
     var svg = d3.select("#vis3plot"+i).append("svg").attr("id", "vis3svg"+i)
@@ -28,13 +28,17 @@ function drawScatterPlots(perp){
   recommenders = ["BiasedMatrixFactorization","BiPolarSlopeOne","CoClustering","Constant","Constant5","FactorWiseMatrixFactorization","GlobalAverage","ItemAverage","LatentFeatureLogLinearModel","MatrixFactorization","Random","SigmoidSVDPlusPlus","SlopeOne","TimeAwareBaseline","TimeAwareBaselineWithFrequencies","UserAverage","UserItemBaseline"]
   
   // Get the data
-  d3.csv("../data/user_2d.csv", function(error, dataAll) {
-    if (error) throw error;
+  d3.csv("../data/user_2d.csv").on("progress", function(evt) {
+        console.log("Amount loaded: " + evt.loaded)
+    })
+    .get(function(error,dataAll) {        
+    
+    if (error) throw error;    
+
     dataAll = dataAll.filter(function(r){ return r.perplexity == perp; })
     for (var i = 0; i < recommenders.length; i++) {
       rec = recommenders[i]
-      data = dataAll.filter(function(r){return r.RS ===rec})
-      // data = data.filter(function(r){return r.perplexity ===perplexity})
+      data = dataAll.filter(function(r){return r.RS ===rec})      
 
       svgs[i].append("text")
             .attr("x", (width / 2))             
@@ -68,65 +72,68 @@ function drawScatterPlots(perp){
           // .attr("transform", "translate("+width+",0)")
           .call(d3.axisLeft(y));
       }
-  });
+  });  
 }
 
 var plotVisualization3 = function() {
   console.log("Plotting Visualization 3.");
   // Interation functionality 
-  // var svgSlider = d3.select("#sliderForPerplexity")
-  //     margin = {right: 50, left: 50},
-  //     width = +svgSlider.attr("width") - margin.left - margin.right,
-  //     height = +svgSlider.attr("height");
+  var svgSlider = d3.select("#sliderForPerplexity")
+      margin = {right: 50, left: 50},
+      width = +svgSlider.attr("width") - margin.left - margin.right,
+      height = +svgSlider.attr("height");
 
-  // svgSlider.append("text")
-  //   .text("t-sne Perplexity:")
-  //   .attr("x", 10)
-  //   .attr("y", 60)
-  //   .attr("font-family", "sans-serif")
-  //   .attr("font-size", "18px")
-  //   .attr("fill", "black");
-  // var x = d3.scaleLinear()
-  //     .domain([5, 50])
-  //     .range([0, width])
-  //     .clamp(true);
+  svgSlider.append("text")
+    .text("t-sne Perplexity:")
+    .attr("x", 10)
+    .attr("y", 60)
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "18px")
+    .attr("fill", "black");
+  var x = d3.scaleLinear()
+      .domain([5, 50])
+      .range([0, width])
+      .clamp(true);
 
-  // var slider = svgSlider.append("g")
-  //     .attr("class", "slider")
-  //     .attr("transform", "translate(" + margin.left + "," + height / 2 + ")");
+  var slider = svgSlider.append("g")
+      .attr("class", "slider")
+      .attr("transform", "translate(" + margin.left + "," + height / 2 + ")");
 
-  // slider.append("line")
-  //     .attr("class", "track")
-  //     .attr("x1", x.range()[0])
-  //     .attr("x2", x.range()[1])
-  //   .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
-  //     .attr("class", "track-inset")
-  //   .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
-  //     .attr("class", "track-overlay")
-  //     .call(d3.drag()
-  //         .on("start.interrupt", function() { slider.interrupt(); })
-  //         .on("start drag", function() { position(x.invert(d3.event.x)); }));
+  slider.append("line")
+      .attr("class", "track")
+      .attr("x1", x.range()[0])
+      .attr("x2", x.range()[1])
+    .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+      .attr("class", "track-inset")
+    .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+      .attr("class", "track-overlay")
+      .call(d3.drag()
+          .on("start.interrupt", function() { slider.interrupt(); replot(x.invert(d3.event.x))})
+          .on("start drag", function() { position(x.invert(d3.event.x)); }));
 
-  // slider.insert("g", ".track-overlay")
-  //     .attr("class", "ticks")
-  //     .attr("transform", "translate(0," + 18 + ")")
-  //     .selectAll("text")
-  //     .data(x.ticks(10))
-  //     .enter().append("text")
-  //     .attr("x", x)
-  //     .attr("text-anchor", "middle")
-  //     .text(function(d) { return d});
+  slider.insert("g", ".track-overlay")
+      .attr("class", "ticks")
+      .attr("transform", "translate(0," + 18 + ")")
+      .selectAll("text")
+      .data(x.ticks(10))
+      .enter().append("text")
+      .attr("x", x)
+      .attr("text-anchor", "middle")
+      .text(function(d) { return d});
 
-  // var handle = slider.insert("circle", ".track-overlay")
-  //     .attr("class", "handle")
-  //     .attr("r", 9);
+  var handle = slider.insert("circle", ".track-overlay")
+      .attr("class", "handle")
+      .attr("r", 9);
 
-  // function position(h) {
-  //   perplexity = String(Math.floor(h))
-  //   drawScatterPlots("50");  
-  //   value = Math.max(Math.min(x(h),300),0)  
-  //   handle.attr("cx", value);  
-  // }
+  function position(h) {    
+    value = Math.max(Math.min(x(h),300),0)  
+    handle.attr("cx", value);  
+  }
+  function replot(h){
+    perplexity = String(Math.floor(h))
+    console.log(perplexity)
+    drawScatterPlots(perplexity);  
+  }
 
-  drawScatterPlots("50");
+  drawScatterPlots("5");
 };
